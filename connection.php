2f29575229c39914059b6f87e974d470b52324b4
nexteveadmin <arunas8115@gmail.com>
2017-02-22 19:04:09 +0530

    deleted unwanted files

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","admin","db_nexteves_admin");
+$con = mysqli_connect("localhost","root","armc123*","db_nexteves_admin");
 if(!$con)
 {
 	die("Error:".mysqli_error($con));
